1a4f3051638a3b1570f22bc61a414aeb506f025e
nonintuitive <lunderw3@gmail.com>
2013-04-18 09:34:43 -0500

    new file:   .openshift/themes/wpbootstrap/footer.php 	new file:   .openshift/themes/wpbootstrap/header.php 	modified:   .openshift/themes/wpbootstrap/index.php 	new file:   .openshift/themes/wpbootstrap/sidebar.php 	modified:   .openshift/themes/wpbootstrap/style.css

---
+++ .openshift/themes/wpbootstrap/footer.php
@@ -0,0 +1,16 @@
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2012</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="bootstrap/js/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
