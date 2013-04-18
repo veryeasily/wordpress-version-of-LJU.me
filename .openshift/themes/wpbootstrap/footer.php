375dbda7aacabcf03f84b188c51786939a14efd2
nonintuitive <lunderw3@gmail.com>
2013-04-18 09:39:26 -0500

    modified:   .openshift/themes/wpbootstrap/footer.php 	new file:   .openshift/themes/wpbootstrap/functions.php 	modified:   .openshift/themes/wpbootstrap/header.php

--- .openshift/themes/wpbootstrap/footer.php
+++ .openshift/themes/wpbootstrap/footer.php
@@ -1,16 +1,12 @@
-      <hr>
+	<hr>
 
       <footer>
-        <p>&copy; Company 2012</p>
+        <p>© Company 2012</p>
       </footer>
 
     </div> <!-- /container -->
 
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="bootstrap/js/jquery.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <?php wp_footer(); ?>
 
   </body>
 </html>
