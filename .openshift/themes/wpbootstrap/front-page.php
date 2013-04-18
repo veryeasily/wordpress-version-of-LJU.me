2a91eb9f7b291d475f18b129f4e34dfacee39274
nonintuitive <lunderw3@gmail.com>
2013-04-18 09:50:17 -0500

    modified:   .openshift/themes/wpbootstrap/front-page.php

--- .openshift/themes/wpbootstrap/front-page.php
+++ .openshift/themes/wpbootstrap/front-page.php
@@ -1,7 +1,13 @@
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php get_header(); ?>
 
-	<?php the_content(); ?>
 
-<?php endwhile; else: ?>
-	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-<?php endif; ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	  	<?php the_content(); ?>
+
+	<?php endwhile; else: ?>
+		<p><?php _e('Sorry, this page does not exist.'); ?></p>
+	<?php endif; ?>
+
+
+
+<?php get_footer(); ?>
