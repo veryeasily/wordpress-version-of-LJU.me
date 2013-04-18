4cb1dd3370a671021f4fcec84b830789cf56f58e
nonintuitive <lunderw3@gmail.com>
2013-04-18 11:40:00 -0500

    modified:   .openshift/themes/wpbootstrap/functions.php 	new file:   .openshift/themes/wpbootstrap/introjs/css/introjs-ie.css 	new file:   .openshift/themes/wpbootstrap/introjs/css/introjs.css 	new file:   .openshift/themes/wpbootstrap/introjs/js/intro.js 	modified:   .openshift/themes/wpbootstrap/style.css

--- .openshift/themes/wpbootstrap/functions.php
+++ .openshift/themes/wpbootstrap/functions.php
@@ -4,8 +4,10 @@ function wpbootstrap_scripts_with_jquery()
 {
 	// Register the script like this for a theme:
 	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/bootstrap.js', array( 'jquery' ) );
+	wp_register_script( 'intro-js', get_template_directory_uri() . 'introjs/js/intro.js' );
 	// For either a plugin or a theme, you can then enqueue the script:
 	wp_enqueue_script( 'custom-script' );
+	wp_enqueue_script( 'intro-js' );
 }
 add_action( 'wp_enqueue_scripts', 'wpbootstrap_scripts_with_jquery' );
 
