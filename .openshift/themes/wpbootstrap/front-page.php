1823d37005bed72998670b779fbf6f4beb766052
nonintuitive <lunderw3@gmail.com>
2013-04-18 20:39:35 -0500

    modified:   .openshift/themes/wpbootstrap/front-page.php 	modified:   .openshift/themes/wpbootstrap/header.php

--- .openshift/themes/wpbootstrap/front-page.php
+++ .openshift/themes/wpbootstrap/front-page.php
@@ -8,6 +8,14 @@
 		<p><?php _e('Sorry, this page does not exist.'); ?></p>
 	<?php endif; ?>
 
+              <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('header')) : else : ?>
+
+              <div class="pre-widget">
+                  <p><strong>Widgetized Header</strong></p>
+                  <p>This panel is active and ready for you to add some widgets via the WP Admin</p>
+              </div>
+
+              <?php endif; ?>
 
 
 <?php get_footer(); ?>
--- .openshift/themes/wpbootstrap/header.php
+++ .openshift/themes/wpbootstrap/header.php
@@ -38,18 +38,6 @@
 			  </ul>
 			</div><!--/.nav-collapse -->
 		  </div>
-		  <div id="widgetized-header">
-
-			  <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('header')) : else : ?>
-
-			  <div class="pre-widget">
-				  <p><strong>Widgetized Header</strong></p>
-				  <p>This panel is active and ready for you to add some widgets via the WP Admin</p>
-			  </div>
-
-			  <?php endif; ?>
-
-		  </div>
 		</div>
 	  </div>
 	</div>
